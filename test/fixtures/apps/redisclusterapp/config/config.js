8b440def37bc5597b3dfa728328073c310d2243c
方正 <215566435@qq.com>
2018-04-05 00:31:17 +1200

    add cluster ci

--- test/fixtures/apps/redisclusterapp/config/config.js
+++ test/fixtures/apps/redisclusterapp/config/config.js
@@ -1,21 +1,48 @@
-'use strict';
+'use strict'
 
 exports.redis = {
-  client: {
-    cluster: true,
-    nodes: [{
-      host: '127.0.0.1',
-      port: 7000,
-      password: '',
-      db: '0',
-    }, {
-      host: '127.0.0.1',
-      port: 7001,
-      password: '',
-      db: '0',
-    }]
-  },
-  agent: true
-};
+    client: {
+        cluster: true,
+        nodes: [
+            {
+                host: '127.0.0.1',
+                port: 7000,
+                password: '',
+                db: '0'
+            },
+            {
+                host: '127.0.0.1',
+                port: 7001,
+                password: '',
+                db: '0'
+            },
+            {
+                host: '127.0.0.1',
+                port: 7002,
+                password: '',
+                db: '0'
+            },
+            {
+                host: '127.0.0.1',
+                port: 7003,
+                password: '',
+                db: '0'
+            },
+            {
+                host: '127.0.0.1',
+                port: 7004,
+                password: '',
+                db: '0'
+            },
+            {
+                host: '127.0.0.1',
+                port: 7005,
+                password: '',
+                db: '0'
+            }
+        ]
+    },
+    agent: true
+}
 
-exports.keys = 'keys';
+exports.keys = 'keys'
--- test/redis.test.js
+++ test/redis.test.js
@@ -1,27 +1,44 @@
-'use strict';
+'use strict'
 
-const mm = require('egg-mock');
-const request = require('supertest');
+const mm = require('egg-mock')
+const request = require('supertest')
 
 describe('test/redis.test.js', () => {
+    describe('single client', () => {
+        let app
+        before(function*() {
+            app = mm.app({
+                baseDir: 'apps/redisapp'
+            })
+            yield app.ready()
+        })
+        after(() => app.close())
+        afterEach(mm.restore)
 
-  describe('single client', () => {
-    let app;
-    before(function* () {
-      app = mm.app({
-        baseDir: 'apps/redisapp',
-      });
-      yield app.ready();
-    });
-    after(() => app.close());
-    afterEach(mm.restore);
+        it('should query', () => {
+            return request(app.callback())
+                .get('/')
+                .expect(200)
+                .expect('bar')
+        })
+    })
 
-    it('should query', () => {
-      return request(app.callback())
-        .get('/')
-        .expect(200)
-        .expect('bar');
-    });
+    describe('cluster client', () => {
+        let app
+        before(async () => {
+            app = mm.app({
+                baseDir: 'apps/redisclusterapp'
+            })
+            await app.ready()
+        })
+        after(() => app.close())
+        afterEach(mm.restore)
 
-  });
-});
+        it('should query', () => {
+            return request(app.callback())
+                .get('/')
+                .expect(200)
+                .expect('bar')
+        })
+    })
+})
